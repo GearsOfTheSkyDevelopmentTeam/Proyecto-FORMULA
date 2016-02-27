488e8819d136a28b06dcc14d01ba265e12d55599
Victor Tortolero <victormtortolero@gmail.com>
2016-02-27 16:44:49 -0430

    Added projectors but still need work

--- formula1/Assets/scripts/EtiquetaPrecio.cs
+++ formula1/Assets/scripts/EtiquetaPrecio.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 
 public class EtiquetaPrecio : MonoBehaviour{
 
@@ -10,15 +11,20 @@ public class EtiquetaPrecio : MonoBehaviour{
 
 	static Dictionary<string, Color> colores;
 	static Dictionary<string, float> costos;
+//	static Dictionary<string, Texture2D> imagenesCostos;
+
+	private Object thisLock = new Object();
 
 	public string nombreCosto;
 	List<Renderer> rdrs;
+
 
 	void Start(){
 
 		rdrs = new List<Renderer>();
 		rdrs.Add(transform.Find("Plane1").GetComponent<Renderer>());
 		rdrs.Add(transform.Find("Plane2").GetComponent<Renderer>());
+
 
 		foreach(Renderer r in rdrs){
 			r.material.SetColor("_TintColor", Color.clear);
@@ -63,20 +69,24 @@ public class EtiquetaPrecio : MonoBehaviour{
 	}
 
 	void InicializarColores(){
-		if(colores == null || costos == null){
-			if(colores == null || costos == null){
+		lock(thisLock){
+			if(colores == null || costos == null /*|| imagenesCostos == null*/){
 				colores = new Dictionary<string, Color>();
 				costos = new Dictionary<string, float>();
+//				imagenesCostos = new Dictionary<string, Texture2D>();
 
-				colores.Add("Diamond", new Color(0, 0, .70f, .60f));
-				colores.Add("Platinum", Color.cyan - new Color(0, 0, 0, .4f));
-				colores.Add("Gold", Color.yellow - new Color(0, 0, 0, .4f));
-				colores.Add("Silver", Color.white - new Color(0, 0, 0, .4f));
+//				imagenesCostos.Add("transparente", Resources.Load("Logos/clear", typeof(Texture2D)) as Texture2D);
+//				imagenesCostos.Add("ups", Resources.Load("Logos/UPS", typeof(Texture2D)) as Texture2D);
 
-				costos.Add("Diamond", 1800000);
-				costos.Add("Platinum", 950000);
-				costos.Add("Gold", 600000);
-				costos.Add("Silver", 300000);
+				colores.Add ("Diamond", new Color (0, 0, .70f, .60f));
+				colores.Add ("Platinum", Color.cyan - new Color (0, 0, 0, .4f));
+				colores.Add ("Gold", Color.yellow - new Color (0, 0, 0, .4f));
+				colores.Add ("Silver", Color.white - new Color (0, 0, 0, .4f));
+
+				costos.Add ("Diamond", 1800000);
+				costos.Add ("Platinum", 950000);
+				costos.Add ("Gold", 600000);
+				costos.Add ("Silver", 300000);
 			}
 		}
 	}
